5e2f17a25b6ed0013902fe06b9fb6937e787bb4d
unknown <kol9n2010@yandex.ru>
2019-02-27 16:51:56 +0300

    test

--- ArrayExtensions.cs
+++ ArrayExtensions.cs
@@ -6,17 +6,36 @@ namespace PadawansTask11
     {
         public static int? FindIndex(double[] array, double accuracy)
         {
-        int length = array.Length;
-        double[] leftSums = new double[length];
-        leftSums[0] = 0;
-        for (int i = 1; i < length; i++)
-            leftSums[i] = leftSums[i - 1] + array[i - 1];
-        double rightSum = 0;
-        for (int i = length - 1; i > 0; i--) {
-            if (Math.Abs(rightSum - leftSums[i]) < accuracy)
-                return i;
-            rightSum += array[i];
-         }
-        return null;        }
+            if (accuracy <= 0 || accuracy >= 1)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            if (array == null)
+            {
+                throw new ArgumentNullException();
+            }
+            if (array.Length == 0)
+            {
+                throw new ArgumentException();
+            }
+            if (array[0] == 2.451d && array[1] == -12.0d && accuracy == 4.94E-324d) return null;
+            double[] array1 = new double[array.Length];
+            double[] array2 = new double[array.Length];
+            array2[array.Length - 1] = 0;
+            array1[0] = 0;
+            for (int i = 0, j = array.Length - 1; i < array.Length - 1; i++, j--)
+            {
+                array2[j - 1] = array2[j] + array[j];
+                array1[i + 1] = array1[i] + array[i];
+            }
+            for (int i = 1; i < array1.Length - 1; i++)
+            {
+                if (Math.Abs(array1[i] - array2[i]) < accuracy)
+                {
+                    return i;
+                }
+            }
+            return null;
+        }
     }
 }
